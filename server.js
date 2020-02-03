7b71d1d96b5c97744d6a233ae582683d5ce2846e
Lucy <lucy13palmer7@gmail.com>
2020-02-02 20:26:08 -0700

    json packs,server.js & sheme.js added

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const port = process.env.port || 3000;
+const app = express();
+// Middleware
+app.use(
+  express.static("public", () => {
+    console.log("this is middleware running");
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  userNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// Routes
+app.use(require("./routes/routes"));
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
