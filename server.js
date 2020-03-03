328229cf02c80d8bad65051a8bd1cfa735933bcb
Lucy <lucy13palmer7@gmail.com>
2020-03-02 19:40:41 -0700

    fixed port to PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const port = process.env.port || 27017;
+const port = process.env.PORT || 27017;
 const app = express();
 // Middleware
 app.use(express.static("public"));
